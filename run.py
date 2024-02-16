39960ea479c2be0d937ec28b52ea12b9a21152b6
David Anderson <davidanderson@live.ie>
2024-02-16 22:17:05 +0000

    Fix typo

--- run.py
+++ run.py
@@ -196,7 +196,7 @@ def new_game(username):
 
     clear()
     print(f"\nGame Over! Your score was {score}/{len(questions)}")
-    print(f"\nYour score will not be updated to the scoresheet.")
+    print(f"\nYour score will be updated to the scoresheet.")
     update_scoresheet(username, score)
     input("Press Enter to return to the menu..")
     """
